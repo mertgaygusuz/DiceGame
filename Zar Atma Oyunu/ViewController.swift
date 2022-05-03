3f75d434f8cc67b4f9df8e3a7675f28c2e106c0f
Mert Gaygusuz <mertgaygusuz@hotmail.com>
2022-05-03 17:43:35 +0300

    Initial Commit

---
+++ Zar Atma Oyunu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zar Atma Oyunu
+//
+//  Created by Mert Gaygusuz on 3.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
